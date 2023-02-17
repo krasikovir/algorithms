05242c47e9aa328b999fc7b9becfb168fc0f0bf8
Igor Krasikov <krasikovir@gmail.com>
2023-02-17 15:41:18 +0500

    ahocorasick

--- aho_corasick.cpp
+++ aho_corasick.cpp
@@ -1,68 +1,63 @@
-#include <bits/stdc++.h>
 using namespace std;
-
-unordered_map <int, vector <string>> um;
-
-struct node {
-	int next[26], go[26];
-	bool term = false;
-	int p = -1, link = -1;
-	char pch;
-
-	node (int p = -1, char pch = '#') : p(p), pch(pch) {
-		fill(next, next + 26, -1);
-		fill(go, go + 26, -1);
-	}
-};
-
-vector <node> trie = {node()};
-
-void add(string & s) {
-	int v = 0;
-	for (char ch : s) {
-		if (trie[v].next[ch - 'a'] == -1) {
-			trie[v].next[ch - 'a'] = trie.size();
-			trie.emplace_back(v, ch);
-		}
-		v = trie[v].next[ch - 'a'];
-	}
-	trie[v].term = true;
-	um[v].push_back(s);
-}
-
-int go(int v, char ch);
-
-int getlink(int v) {
-	if (trie[v].link == -1) {
-		if (v == 0 || trie[v].p == 0) trie[v].link = 0;
-		else trie[v].link = go(getlink(trie[v].p), trie[v].pch);
-	}
-	return trie[v].link;
-}
-
-int go(int v, char ch) {
-	if (trie[v].go[ch - 'a'] == -1) {
-		if (trie[v].next[ch - 'a'] != -1) trie[v].go[ch - 'a'] = trie[v].next[ch - 'a'];
-		else trie[v].go[ch - 'a'] = (v == 0 ? 0 : go(getlink(v), ch));
-	}
-	return trie[v].go[ch -  'a'];
-}
+#include <bits/stdc++.h>
 
 int main() {
-	vector <string> dict = {"he", "she", "hers", "his"};
-	string text = "ahishers";
+    vector <string> dict = {
+		"ahoc",
+		"hoc",
+		"cora",
+		"corr"
+    };
+    string text = "ahocorasick";
 
-	for (auto x : dict) add(x);
+    int n = 10; for (auto x : dict) n += x.size();
+    int fail[n], trie[n][26];
+    unordered_map <int, unordered_set <string>> word;
 
-	for (int i = 0, c = 0; i < text.size(); ++i) {
+    memset(trie, -1, sizeof trie);
+    memset(fail, -1, sizeof fail);
 
-		// if vertex is terminal (c <- vertex) than print all the words ended here
-		c = go(c, text[i]);
-		if (um.count(c)) {
-			for (auto x : um[c]) cout << x << " ";
-			cout << "\n";
-		}
-	}
-	return 0;
+    // c <-- counter (root <- 0)
+    // s <-- state
+    for (int i = 0, c = 0, s = 0; i < dict.size(); ++i, s = 0) {
+        for (int j = 0; j < dict[i].size(); ++j) {
+            if (trie[s][dict[i][j] - 'a'] == -1) trie[s][dict[i][j] - 'a'] = ++c;
+            s = trie[s][dict[i][j] - 'a'];
+        }
+        word[s].insert(dict[i]);
+    }
+
+    // link root children to root
+    for (int i = 0; i < 26; ++i) if (trie[0][i] == -1) trie[0][i] = 0;
+
+    // put existent root children to queue
+    queue <int> q;
+    for (int i = 0; i < 26; ++i) if (trie[0][i] != 0) fail[trie[0][i]] = 0, q.push(trie[0][i]);
+
+    for (;q.size();) {
+        int s = q.front(); q.pop();
+        for (int i = 0; i < 26; ++i) {
+            if (trie[s][i] != -1) {
+                int f = fail[s];
+                for (;trie[f][i] == -1;) f = fail[f];
+                f = trie[f][i];
+                fail[trie[s][i]] = f;
+                for (auto x : word[f]) word[trie[s][i]].insert(x);
+                q.push(trie[s][i]);
+            }
+
+        }
+    }
+
+    for (int i = 0, c = 0; i < text.size(); ++i) {
+        for (;trie[c][text[i] - 'a'] == -1;) c = fail[c];
+        c = trie[c][text[i] - 'a'];
+        for (int j = 0; j < dict.size(); ++j) {
+            if (word[c].count(dict[j])) {
+                cout << i - dict[j].size() + 1 << " " << i << "\n";
+                cout << dict[j] << "\n\n";
+            }
+        }
+    }
+    return 0;
 }
-

bd68ddb4f28841d032c9653b9b5131c988be0053
Igor Krasikov <krasikovir@gmail.com>
2023-02-22 23:59:44 +0500

    lis fix

--- cpp/lis_nn.cpp
+++ cpp/lis_nn.cpp
@@ -10,7 +10,7 @@ int main() {
 
 	for (int i = 0; i < n; ++i) 
 		for (int j = 0; j < i; ++j) 
-			if (arr[i] > arr[j] && (p[i] == -1 || dp[j] + 1 >= dp[i])) p[i] = j, dp[i] = dp[j] + 1;
+			if (arr[i] > arr[j] && dp[j] + 1 >= dp[i]) p[i] = j, dp[i] = dp[j] + 1;
 
 	int idx = max_element(dp, dp + n) - dp;
 

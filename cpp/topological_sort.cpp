01dcbb67a9ae354a3b45be5f5a947fa83b1bafda
Igor Krasikov <krasikovir@gmail.com>
2023-02-25 18:07:31 +0500

    topological

---
+++ cpp/topological_sort.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	vector <vector <int>> g(n);
+	int in[n]; memset(in, 0, sizeof in);
+
+	// O(n + m)
+	for (int i = 0; i < n; ++i) for (auto x : g[i]) ++in[x];
+
+	queue <int> q; vector <int> ans;
+	for (int i = 0; i < n; ++i) if (in[i] == 0) q.push(i);
+
+	for (;q.size();) {
+		int v = q.front(); q.pop();
+		ans.push_back(v);
+		for (auto x : g[v]) {
+			--in[x];
+			if (in[x] == 0) q.push(x);
+		}
+	}
+
+	// possibility
+	ans.size() == n;
+
+	return 0;
+}

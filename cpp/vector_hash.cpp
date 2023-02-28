0dd2876774fb59b9c1ceebd3ed527f881454cab4
Igor Krasikov <krasikovir@gmail.com>
2023-02-28 22:13:04 +0500

    hasher

--- cpp/vector_hash.cpp
+++ cpp/vector_hash.cpp
@@ -1,18 +1,19 @@
-include <bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const unsigned long long C = 0x9e3779b9;
 
+template <typename T>
 struct HASHER {
-	unsigned long long operator()(const vector <int> & arr) const {
+	unsigned long long operator()(const vector <T> & arr) const {
 		unsigned long long ans = 0;
-		for (const auto & x : arr) ans ^= hash<int>()(x) + C + (ans << 6) + (ans >> 2);
+		for (const auto & x : arr) ans ^= hash<T>()(x) + C + (ans << 6) + (ans >> 2);
 		return ans;
 	}
 };
 
 int main() {
-	unordered_set <vector <int>, HASHER> us;
+	unordered_set <vector <int>, HASHER<int>> us;
 	us.insert({1, 2, 3});
 	us.insert({2, 2, 8});
 	return 0;

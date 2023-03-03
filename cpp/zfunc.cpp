fd7df166c1148c7d220fa331be75a6f9a4f6ac9f
Igor Krasikov <krasikovir@gmail.com>
2023-03-03 12:42:01 +0500

    z2

--- cpp/zfunc.cpp
+++ cpp/zfunc.cpp
@@ -5,7 +5,7 @@ int main() {
 	int n;
 	string s;
 
-	int z[n]; z[0] = 0;
+	int z[n]; memset(z, 0, sizeof z);
 	for (int i = 1, l = 0, r = 0; i < n; ++i) {
 		if (i <= r) z[i] = min(r - i + 1, z[i - l]);
 		for (;i + z[i] < n && s[z[i]] == s[i + z[i]]; ++z[i]);

0239ae5c27408c9ebb033b45f183e49d4a3d74f1
Igor Krasikov <krasikovir@gmail.com>
2023-03-03 12:30:13 +0500

    z

---
+++ cpp/zfunc.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	string s;
+
+	int z[n]; z[0] = 0;
+	for (int i = 1, l = 0, r = 0; i < n; ++i) {
+		if (i <= r) z[i] = min(r - i + 1, z[i - l]);
+		for (;i + z[i] < n && s[z[i]] == s[i + z[i]]; ++z[i]);
+		if (i + z[i] - 1 > r), l = i, r = i + z[i] - 1;
+	}
+
+	return 0;
+}
